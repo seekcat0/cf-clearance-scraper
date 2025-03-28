f9905f5362feb0ae21652a623d7b52f538cb20d6
Seek Cat <seekcat033@gmail.com>
2025-03-28 22:30:03 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 10000
 const bodyParser = require('body-parser')
 const authToken = process.env.authToken || null
 const cors = require('cors')
